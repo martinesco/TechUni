3a635df4136233ada67186c089e67d5b7a1530a9
Martin Georgiev <martngeorgiev@gmail.com>
2020-04-21 23:39:34 +0300

    Added Model–View–ViewModel

---
+++ Programni Sredi/WpfExample/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfExample
+{
+    public class MainWindowViewModel
+    {
+       /* public string ButtonContent
+        {
+            get
+            {
+                return "Click Me";
+            }
+        }*/
+
+        private ICommand hiButtonCommand;
+        private ICommand toggleExecuteCommand
+        { get; set; }
+        private bool canExecute = true;
+        public string HiButtonContent
+        {
+            get { return "click to hi"; }
+        }
+        public bool CanExecute
+        {
+            get { return this.canExecute; }
+            set
+            {
+                if (this.canExecute == value)
+                { return; }
+                this.canExecute = value;
+            }
+        }
+        public ICommand ToggleExecuteCommand
+        {
+            get
+            { return toggleExecuteCommand; }
+            set
+            { toggleExecuteCommand = value; }
+        }
+        public ICommand HiButtonCommand
+        {
+            get
+            { return hiButtonCommand; }
+            set
+            { hiButtonCommand = value; }
+        }
+        public MainWindowViewModel()
+        {
+            HiButtonCommand = new RelayCommand(ShowMessage, param => this.canExecute);
+            toggleExecuteCommand = new RelayCommand(ChangeCanExecute);
+        }
+        public void ShowMessage(object obj)
+        {
+            //it is good we do this with binding to some control
+            System.Windows.MessageBox.Show(obj.ToString());
+        }
+        public void ChangeCanExecute(object obj)
+        {
+            canExecute = !canExecute;
+        }
+    }
+}
